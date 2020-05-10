9aedcde8f4dac26d998fb78bebb67704c7d830d4
Layndo Sfr <layndoaqsa17@gmail.com>
2020-05-10 07:26:00 +0700

    avatar

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -64,7 +64,7 @@ class DefaultHeader extends Component {
           </NavItem> */}
           <UncontrolledDropdown nav direction="down">
             <DropdownToggle nav>
-              <img src={'../../assets/img/avatars/6.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" />
+              <img src={'../../assets/img/avatars/avatar.png'} className="img-avatar" alt="avatar.png" />
             </DropdownToggle>
             <DropdownMenu right>
               {/* <DropdownItem header tag="div" className="text-center"><strong>Account</strong></DropdownItem>
